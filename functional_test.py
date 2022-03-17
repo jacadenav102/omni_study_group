13f3e2ac8437de01b35bc3d2ce6c7f958e603044
jose_cadena <jose.cadena@omnilatam.com>
2022-03-17 14:10:23 -0500

    Feature Django project start

---
+++ functional_test.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+browser = webdriver.Remote(
+   command_executor='http://127.0.0.1:4444/wd/hub',
+   desired_capabilities=DesiredCapabilities.CHROME)
+
+browser.get('http://127.0.0.1:8000/')
+assert 'Django' in browser.title
