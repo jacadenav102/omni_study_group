20a1a34ebe14ca1ee6a8d5463cba98096455153f
jose_cadena <jose.cadena@omnilatam.com>
2022-03-17 18:49:01 -0500

    dd

--- functional_test.py
+++ functional_test.py
@@ -1,9 +1,18 @@
-from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium import webdriver
 
-browser = webdriver.Remote(
-   command_executor='http://127.0.0.1:4444/wd/hub',
-   desired_capabilities=DesiredCapabilities.CHROME)
+import unittest
 
-browser.get('http://127.0.0.1:8000/')
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+   def setUp(self) -> None:
+      self.browser = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
+                                      desired_capabilities=DesiredCapabilities.CHROME)
+   def tearDown(self) -> None:
+      self.browser.quit()
+   
+   def test_can_start_a_list_and_retrieve_it_later(self):
+      self.browser.get('http://127.0.0.1:8000/')
+      self.assertIn("invalid session id", self.browser.title)
+#      self.fail('Finish the test!')
+if __name__ == "__main__":
+   unittest.main(warnings='ignore')
